409535ad3b2488c57a6244e3a894b1f74bb4acfa
tslater2006 <tslater2006@55b5ed49-161a-1e82-b1c3-dd93baa6d912>
2010-08-23 19:43:40 +0000

    Preparing to work on last 2 AI's This is a stable build

---
+++ androidyaniv/src/com/andro/yaniv/ai/InsaneAI.java
@@ -0,0 +1,21 @@
+package com.andro.yaniv.ai;
+
+import com.andro.yaniv.game.Player;
+import com.andro.yaniv.game.PlayerHand;
+import com.andro.yaniv.game.PlayingCard;
+
+public class InsaneAI extends YanivAI {
+
+	@Override
+	public PlayingCard[] getBestDrop(Player curPlayer) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getBestPickup(PlayerHand discardHand) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
